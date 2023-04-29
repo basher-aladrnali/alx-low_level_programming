6e6e78f9365407968c1a1f4e0f3b3ff8b287d4f0
basher <basheraladrnali@gmail.com>
2023-04-29 14:18:07 -0700

    basher

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c/102-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
+
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+
